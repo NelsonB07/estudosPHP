52dc0b448341857a5994bc0ba586328359ace7a3
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2025-10-27 01:55:49 -0300

    Estudos PHP

---
+++ array_sum/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+    $arr = [2, 4, 34, 55, 634, 523, 6, 77, 100];
+
+    $soma = array_sum($arr);
+    print_r($arr);
+    echo "<br>";
+    echo $soma;
+
+
+
+?>
---
+++ unindo_arrays/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+    $arr1 = [1, 2, 3];
+    $arr2 = [10, 40, 90];
+    $arr3 = [2.1, 44.5, 43.3];
+    $arr4 = ["Nelson", "Brandão", "Junior"];
+
+    $arrMerge = array_merge($arr1, $arr2, $arr3, $arr4, [0, 1]);
+
+    print_r($arrMerge);
+    echo "<br>";
+
+
+
+?>
