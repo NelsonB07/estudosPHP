4c65422de27140c74dc4c19dd70cc6e2fdf1fec9
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2025-11-12 02:45:43 -0300

    classes abstratas

---
+++ classes_abstratas/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+    abstract class Teste {
+
+        public static function testandoClasse () {
+            echo "Este método é de uma classe abstrata <br>";
+        }
+
+        abstract public function testeAbs(); 
+    }
+
+    // $t = new Teste;
+
+    Teste::testandoClasse();
+
+    class Nova extends teste {
+
+        public function testeAbs() {
+            echo "Teste método abstrato <br>";
+        }
+    }
+
+    $n = new Nova;
+    $n->testeAbs();
+
+
+
+
+?>
