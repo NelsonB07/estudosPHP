03eedce2dfd5468774d0507ae0151686840efb9d
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2025-11-19 02:59:25 -0300

    Estudos HTTP

---
+++ get/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário GET</title>
+</head>
+<body>
+    <form action="processamento.php" method="get">
+        <div>
+            <input type="text" name="nome" id="" placeholder="Preencha seu nome">
+        </div>
+        <div>
+            <input type="text" name="idade" id="" placeholder="Preencha sua idade">
+        </div>
+        <div>
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
+</body>
+</html>
