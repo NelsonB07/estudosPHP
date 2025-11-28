9b1cea9baf9fbb07d751140c0426dbe8f9712798
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2025-11-28 01:41:17 -0300

    Estudos PHP, Sessions

---
+++ session/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+    session_start();
+
+    // print_r($_SESSION);
+
+    $_SESSION['nome'] = "Nelson";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session</title>
+</head>
+<body>
+    <h1>Olá, Session.</h1>
+</body>
+</html>
