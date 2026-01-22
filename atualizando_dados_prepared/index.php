bfe00d6f66d23a1a0f0d43fa5a8c5e6cd74ebaa0
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2026-01-22 16:33:27 -0300

    Estudo de UPDATE seguro usando prepared statements

---
+++ atualizando_dados_prepared/index.php
@@ -0,0 +1,43 @@
+<?php 
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "cursophp";
+
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    // ASSUNTO DA AULA
+
+    $id = 14;
+
+    $stmt = $conn->prepare("UPDATE itens SET nome = ?, descricao = ? WHERE id = ?");
+
+    $nome = "Sofá";
+    $descricao = "Sofá semi novo, com madeira de demolição";
+
+    $stmt->bind_param("ssi", $nome, $descricao, $id);
+
+    $stmt->execute();
+
+    if($stmt->error) {
+        echo "Erro: " . $stmt->error;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
